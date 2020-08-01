7d53fbeccbfc952ba767885e524812f4b51c4355
esterlilianlb <esterlilian2231@gmail.com>
2020-08-01 12:11:12 -0300

    setup inicial

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -3,15 +3,24 @@
 
 (defn click-counter [click-count]
   [:div
-   "The atom " [:code "click-count"] " has value: "
+   "O botão " [:code "contador"] " está com o valor: "
    @click-count ". "
-   [:input {:type "button" :value "Click me!"
+   [:input {:type "button" :value "Click me!" :class "button"
             :on-click #(swap! click-count inc)}]])
+
+(defn prompt [])
+
 
 (def click-count (r/atom 0))
 
 (defn hello []
   [:<>
-   [:p "Hello, first-cljs-page is running!"]
-   [:p "Here's an example of using a component with state:"]
-   [click-counter click-count]])
+   [:p "Oi, essa é minha primeira página com ClojureScript!"]
+   [:p "Este é um exemplo de componente usando estado:"]
+   [click-counter click-count]
+   [:h1 "aqui tenho umformulário simples"]
+   [:form
+   [:p "Meu formulário"]
+   [:input {:type "text" :placeholder "seu nome aqui"}]
+   [:input {:type "submit" :value "Enviar" :class "button"}]
+   ]])
